94033c959d14ff62aed88942918be38d5c95ab23
yckim980318 <yckim980318@gmail.com>
2024-01-12 17:32:51 +0900

    Add Container, text

---
+++ src/styles/CommonStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    paddingTop: 27,
+    fontSize: 18,
+  },
+});
+
+export default commonStyles;
