6cd2246848e31bc6bb69fde4f44c43f5a55818ee
yckim980318 <yckim980318@gmail.com>
2024-01-12 17:12:16 +0900

    Seperated StyleSheet

--- src/screens/EnterScreen.js
+++ src/screens/EnterScreen.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
+import EnterScreenStyles from '../styles/EnterScreenStyles';
 
 const EnterScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Real</Text>
-      <Text style={styles.text}>Voice</Text>
+    <View style={EnterScreenStyles.container}>
+      <Text style={EnterScreenStyles.text}>Real</Text>
+      <Text style={EnterScreenStyles.text}>Voice</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 50,
-    fontWeight: 'bold',
-  },
-});
 
 export default EnterScreen;
---
+++ src/styles/EnterScreenStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const EnterScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+});
+
+export default EnterScreenStyles;
