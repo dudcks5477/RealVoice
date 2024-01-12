5b1d9619f57db00a8257e739d2ce2fbe83433a0c
yckim980318 <yckim980318@gmail.com>
2024-01-12 17:31:24 +0900

    Add NumberScreen

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import EnterScreen from './src/screens/EnterScreen';
+import NumberScreen from './src/auth/NumberScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Enter" component={EnterScreen} />
+        <Stack.Screen
+          name="Number"
+          component={NumberScreen} // NumberScreen 추가
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
