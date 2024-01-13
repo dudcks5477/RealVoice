2a4852f8de23f08559e588cc788f0a24f0e18f56
dudcks5477 <rladudcks5477@naver.com>
2024-01-13 13:45:24 +0900

    Add Phone Number logic

--- src/auth/NumberScreen.js
+++ src/auth/NumberScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import CommonStyles from '../styles/CommonStyles';
+import NumberInput from './NumberInput';
 
 const NumberScreen = () => {
+  const handlePhoneNumberSubmit = phoneNumber => {
+    // 입력된 전화번호를 처리하는 로직을 여기에 추가
+    console.log('전화번호:', phoneNumber);
+  };
   return (
     <View style={CommonStyles.container}>
       <Text style={CommonStyles.text}>전화번호가 어떻게 되세요?</Text>
+      <NumberInput onSubmit={handlePhoneNumberSubmit} />
     </View>
   );
 };
