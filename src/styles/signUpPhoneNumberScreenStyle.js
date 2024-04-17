e891486f3414bb9df86f36fd2704085dd27ac25f
dudcks5477 <rladudcks5477@naver.com>
2024-04-17 16:27:46 +0900

    page Styling

---
+++ src/styles/signUpPhoneNumberScreenStyle.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+const signUpPhoneNumberScreenStyle = StyleSheet.create({
+  phoneNumber: {
+    height: 40,
+    width: '95%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    color: '#000',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  country: {
+    height: 50,
+    width: '100%',
+    marginBottom: 10,
+  },
+});
+export default signUpPhoneNumberScreenStyle;
