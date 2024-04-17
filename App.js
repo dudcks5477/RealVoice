f8c5278381d3c821a27e9ee5dc6f37ad398b292c
dudcks5477 <rladudcks5477@naver.com>
2024-04-17 14:46:09 +0900

    Add Navigator

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import SplashScreen from './src/components/SplashScreen';
+import SignUpPhoneNumberScreen from './src/screens/SignUpPhoneNumberScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <SplashScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen
+          name="SignUpPhoneNumber"
+          component={SignUpPhoneNumberScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
