e3215d85621f7ca25f135985cd4728b2f5891b18
dudcks5477 <rladudcks5477@naver.com>
2024-04-18 15:13:35 +0900

    Styling

---
+++ src/styles/phoneVerificationScreenStyle.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const phoneVerificationScreenStyle = StyleSheet.create({
+  placeholder: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
+export default phoneVerificationScreenStyle;
