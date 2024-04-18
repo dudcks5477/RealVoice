47fcfd1fe10c2feb277eb02d3dbff063e2f15e02
dudcks5477 <rladudcks5477@naver.com>
2024-04-18 15:10:45 +0900

    Sample Page

---
+++ src/screens/MicroPhonePermissionScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MicroPhonePermissionScreen = () => {
+  return (
+    <View>
+      <Text>마이크 활성화{'\n'} 당신의 기분을 말할 수 있습니다.</Text>
+    </View>
+  );
+};
+
+export default MicroPhonePermissionScreen;
