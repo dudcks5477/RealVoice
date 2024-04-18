2d8fcc4ceace94a98cce946458806f6d195cce20
dudcks5477 <rladudcks5477@naver.com>
2024-04-18 15:12:50 +0900

    Modify PhoneVerification name of navigation

--- src/screens/SignUpPhoneNumberScreen.js
+++ src/screens/SignUpPhoneNumberScreen.js
@@ -27,7 +27,7 @@ const SignUpPhoneNumberScreen = () => {
       Alert.alert('전화번호를 입력하세요.');
       return;
     }
-    navigation.navigate('PhoneVerificationScreen');
+    navigation.navigate('PhoneVerification');
   };
 
   const goToTermsOfService = () => {
