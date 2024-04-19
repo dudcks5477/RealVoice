2b67f5f8416b5af13597a9aaa1903e7379eed723
dudcks5477 <rladudcks5477@naver.com>
2024-04-19 16:43:39 +0900

    Sample Page

---
+++ src/screens/NickNameScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const NickNameScreen = () => {
+  return (
+    <View>
+      <Text>당신을 뭐라 부를까요?</Text>
+    </View>
+  );
+};
+
+export default NickNameScreen;
