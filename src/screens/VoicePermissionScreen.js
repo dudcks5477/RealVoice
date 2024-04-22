97aec1a2c0e7b22c49b4a397adf410c8d252fa63
dudcks5477 <rladudcks5477@naver.com>
2024-04-22 15:10:42 +0900

    Sample page

---
+++ src/screens/VoicePermissionScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const VoicePermissionScreen = () => {
+  return (
+    <View>
+      <Text>RealVoice를 언제{'\n'} 들려주실껀가요</Text>
+    </View>
+  );
+};
+
+export default VoicePermissionScreen;
