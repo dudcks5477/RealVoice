7c461f45f5c60e5b1d3ddfaebc7b1a6b75ab6215
dudcks5477 <rladudcks5477@naver.com>
2024-04-22 15:10:17 +0900

    Next btn

--- src/screens/NickNameScreen.js
+++ src/screens/NickNameScreen.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import Common from '../styles/common';
+import nickNameScreenStyle from '../styles/nickNameScreenStyle';
 
 const NickNameScreen = () => {
+  const [nickName, setNickName] = useState('');
+  const navigation = useNavigation();
+
+  const handleNickNameChnage = text => {
+    setNickName(text);
+  };
+
+  const isNickNameValid = nickName.length >= 2;
+
+  const handleNextPress = () => {
+    if (isNickNameValid) {
+      navigation.navigate('VoicePermission');
+    }
+  };
+
   return (
-    <View>
-      <Text>당신을 뭐라 부를까요?</Text>
+    <View style={Common.container}>
+      <Text style={Common.headerText}>당신을 뭐라 부를까요?</Text>
+      <View style={nickNameScreenStyle.inputContainer}>
+        <TextInput
+          style={nickNameScreenStyle.input}
+          placeholder="Real Voice"
+          onChangeText={handleNickNameChnage}
+          value={nickName}
+        />
+        <Text style={Common.text}>
+          두 글자 이상 입력하세요{'\n'}
+          언제든지 변경할 수 있습니다
+        </Text>
+      </View>
+      <View style={nickNameScreenStyle.nextBtnContainer}>
+        <TouchableOpacity
+          style={[
+            nickNameScreenStyle.nextBtn,
+            {opacity: isNickNameValid ? 1 : 0.5},
+          ]}
+          onPress={handleNextPress}
+          disabled={!isNickNameValid}>
+          <Text style={nickNameScreenStyle.nextBtnText}>다음</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
