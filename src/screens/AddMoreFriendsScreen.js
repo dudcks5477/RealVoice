deabfcdb65d99377cfb1807233d141651b0465e8
dudcks5477 <rladudcks5477@naver.com>
2024-04-25 16:56:15 +0900

    Sample Page

---
+++ src/screens/AddMoreFriendsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddMoreFriendsScreen = () => {
+  return (
+    <View>
+      <Text>RealVoice</Text>
+    </View>
+  );
+};
+
+export default AddMoreFriendsScreen;
---
+++ src/screens/RequiredScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RequiredScreen = () => {
+  return (
+    <View>
+      <Text>RealVoice</Text>
+    </View>
+  );
+};
+
+export default RequiredScreen;
