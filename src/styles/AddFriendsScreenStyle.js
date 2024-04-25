7b337b546c4829ee250cecacac5ef87ef58a7e65
dudcks5477 <rladudcks5477@naver.com>
2024-04-25 16:56:43 +0900

    Styling

---
+++ src/styles/AddFriendsScreenStyle.js
@@ -0,0 +1,186 @@
+import {StyleSheet} from 'react-native';
+
+const addFriendsScreenStyle = StyleSheet.create({
+  header: {
+    marginTop: 37,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  iconContainer: {
+    paddingHorizontal: 10,
+  },
+  iconNone: {
+    fontSize: 25,
+    color: 'transparent',
+  },
+  icon: {
+    color: '#fff',
+    fontSize: 25,
+  },
+  search: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchContainer: {
+    width: 370,
+    height: 40,
+    backgroundColor: '#606060',
+    borderRadius: 14,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchIcon: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  searchInput: {
+    color: '#fff',
+    fontSize: 13,
+  },
+  share: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  shareContainer: {
+    width: 370,
+    height: 55,
+    backgroundColor: '#606060',
+    borderRadius: 14,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  Invited: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  nickName: {
+    color: '#fff',
+  },
+  shareIconContainer: {
+    marginLeft: 190,
+  },
+  shareIcon: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  recommand: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  recommandContainer: {
+    width: 370,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  recommandText: {
+    color: '#606060',
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  addFriendContainer: {
+    width: 380,
+    borderRadius: 14,
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'space-between',
+  },
+  circle: {
+    width: 44,
+    height: 44,
+    borderRadius: 52,
+    backgroundColor: '#2a55ee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nickNameContainer: {
+    marginLeft: 10,
+  },
+  button: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  addCancelBtn: {
+    flexDirection: 'row',
+  },
+  addBtn: {
+    backgroundColor: '#d9d9d9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    width: 40,
+  },
+  addText: {
+    color: '#000',
+    fontSize: 13,
+  },
+  cancelBtn: {
+    justifyContent: 'center',
+    marginLeft: 13,
+  },
+  cancelIcon: {
+    color: '#fff',
+    fontSize: 30,
+  },
+  moreDetail: {
+    marginTop: 20,
+    backgroundColor: '#606060',
+    width: 83,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+  invitedFriend: {
+    backgroundColor: '#d9d9d9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    width: 40,
+    height: 30,
+    marginRight: 20,
+  },
+  selectFooter: {
+    marginTop: 20,
+    width: 146,
+    height: 40,
+    padding: 8,
+    borderRadius: 15,
+    backgroundColor: '#606060',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  textRecommandContainer: {
+    width: 39,
+    height: 31,
+    borderRadius: 16,
+    backgroundColor: '#d9d9d9',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footerText: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  textMore: {
+    width: 39,
+    height: 31,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default addFriendsScreenStyle;
