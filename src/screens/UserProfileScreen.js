4f09e72fe07382dd47f8d1f4dba16184e738bcd0
dudcks5477 <rladudcks5477@naver.com>
2024-04-26 16:11:54 +0900

    Sample Page

---
+++ src/screens/UserProfileScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const UserProfileScreen = () => {
+  return (
+    <View>
+      <Text>사용자 프로필 입니다.</Text>
+    </View>
+  );
+};
+
+export default UserProfileScreen;
