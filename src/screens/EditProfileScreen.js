5713a84284f25987ccf89d1e2269498d777305d6
dudcks5477 <rladudcks5477@naver.com>
2024-05-09 16:10:56 +0900

    Make frame

---
+++ src/screens/EditProfileScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const EditProfileScreen = () => {
+  return (
+    <View>
+      <Text>사용자 편집 프로필</Text>
+    </View>
+  );
+};
+
+export default EditProfileScreen;
