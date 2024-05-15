85cf14f719fba45c3a841d7e562e0ae7fe288af3
dudcks5477 <rladudcks5477@naver.com>
2024-05-15 15:46:32 +0900

    Modify about content Width

--- src/styles/signUpPhoneNumberScreenStyle.js
+++ src/styles/signUpPhoneNumberScreenStyle.js
@@ -1,25 +1,29 @@
 import {StyleSheet} from 'react-native';
 
 const signUpPhoneNumberScreenStyle = StyleSheet.create({
+  content: {
+    flex: 1,
+    marginTop: 70,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   phoneNumber: {
     height: 40,
-    width: '95%',
-    borderColor: 'gray',
+    width: 340,
     borderWidth: 1,
     marginBottom: 5,
     paddingHorizontal: 10,
+    textAlign: 'center',
+    borderColor: 'gray',
     backgroundColor: '#fff',
     color: '#000',
   },
   text: {
+    fontSize: 12,
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
-  },
-  country: {
-    height: 50,
-    width: '100%',
-    marginBottom: 10,
   },
 });
 export default signUpPhoneNumberScreenStyle;
