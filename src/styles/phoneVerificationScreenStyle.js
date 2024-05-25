5da95ad82826048c4b0d82f4066a5b9b6377f55d
dudcks5477 <rladudcks5477@naver.com>
2024-05-25 22:35:50 +0900

    Add Input verification code UI

--- src/styles/phoneVerificationScreenStyle.js
+++ src/styles/phoneVerificationScreenStyle.js
@@ -1,6 +1,17 @@
 import {StyleSheet} from 'react-native';
 
 const phoneVerificationScreenStyle = StyleSheet.create({
+  phoneNumber: {
+    height: 40,
+    width: 340,
+    borderWidth: 1,
+    marginBottom: 5,
+    paddingHorizontal: 10,
+    textAlign: 'center',
+    borderColor: 'gray',
+    backgroundColor: '#fff',
+    color: '#000',
+  },
   placeholder: {
     fontSize: 16,
     color: 'gray',
