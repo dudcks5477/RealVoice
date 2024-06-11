898dcd6a359b1477acc6a65a980f2b94ebef7d67
dudcks5477 <rladudcks5477@naver.com>
2024-06-11 14:36:08 +0900

    deleteAccount

--- src/screens/OtherSettingScreen.js
+++ src/screens/OtherSettingScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useNavigation} from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 import Common from '../styles/common';
 import mainScreenStyle from '../styles/mainScreenStyle';
@@ -15,6 +17,47 @@ const OtherSettingScreen = () => {
 
   const handleEditProfile = () => {
     navigation.navigate('EditProfile');
+  };
+
+  const clearAppCache = async () => {
+    try {
+      await AsyncStorage.clear();
+      Alert.alert('캐시 지우기', '모든 음성이 성공적으로 지워졌습니다.');
+    } catch (error) {
+      console.error('Failed to clear the async storage', error);
+    }
+  };
+
+  const deleteAccount = async () => {
+    Alert.alert(
+      '계정 삭제',
+      '계정을 삭제하시겠습니까? 이 작업은 되돌릴 수 없습니다.',
+      [
+        {
+          text: '취소',
+          style: 'cancel',
+        },
+        {
+          text: '삭제',
+          onPress: async () => {
+            try {
+              const response = await axios.delete(
+                'http://10.0.2.2:8000/api/users',
+                {
+                  headers: {
+                    Authorization: 'Bearer YOUR_AUTH_TOKEN',
+                  },
+                },
+              );
+              console.log('계정이 성공적으로 삭제되었습니다.');
+              navigation.navigate('Splash');
+            } catch (error) {
+              console.error('계정 삭제 중 에러 발생:', error);
+            }
+          },
+        },
+      ],
+    );
   };
 
   return (
@@ -31,14 +74,18 @@ const OtherSettingScreen = () => {
       </View>
       <View style={worldTimeScreenStyle.EditBtnContainer}>
         <View style={editProfileScreenStyle.actionContainer}>
-          <TouchableOpacity style={editProfileScreenStyle.actionBtn}>
+          <TouchableOpacity
+            style={editProfileScreenStyle.actionBtn}
+            onPress={clearAppCache}>
             <View style={mainScreenStyle.buttonContainer}>
               <Icon name="cached" style={editProfileScreenStyle.iconCalendar} />
             </View>
             <Text style={editProfileScreenStyle.nickname}>캐시 지우기</Text>
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={otherSettingScreenStyle.logoutContainer}>
+        <TouchableOpacity
+          style={otherSettingScreenStyle.logoutContainer}
+          onPress={deleteAccount}>
           <Text style={editProfileScreenStyle.logoutText}>계정 삭제</Text>
         </TouchableOpacity>
       </View>
